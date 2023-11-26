74a827e3e919a9012072e38b812bc05a0c1ec202
Zakir Hossain <zakir@macbook.local>
2023-11-26 14:30:59 +0800

    Print message before sending to topic

--- src/test/java/com/example/producer/TopicUsersTest.java
+++ src/test/java/com/example/producer/TopicUsersTest.java
@@ -3,6 +3,8 @@ package com.example.producer;
 import com.example.producer.dto.User;
 import com.example.producer.message.Message;
 import com.example.producer.message.UserSchema;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,10 +20,13 @@ class TopicUsersTest {
     @Autowired
     KafkaTemplate<String, Message> kafkaTemplate;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
-    void testProduceMessageToUsers() {
+    void testProduceMessageToUsers() throws JsonProcessingException {
         User user = new User(1493819497170L, "User_10", "Region_5", "MALE");
         Message<User, UserSchema> message = new Message<>(UserSchema.instance, user);
+        System.out.println(objectMapper.writeValueAsString(message));
         kafkaTemplate.send(topicName, message);
     }
 
